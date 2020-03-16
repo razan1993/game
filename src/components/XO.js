e34198543dc8a3ad39ec5eff5ab7788f4e0ad0bd
razanlamdani <razan19931@outlook.sa>
2020-03-16 01:57:10 -0700

    add files

---
+++ src/components/XO.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function XO({ index, myId, myFunction }) {
+  return (
+    <div className="col border-r border" id={myId} onClick={() => myFunction(index)}></div>
+  );
+}
+
+export default XO;
